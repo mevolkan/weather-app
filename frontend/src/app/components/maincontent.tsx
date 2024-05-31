78d1d1557351333dc0d77dcb99412ea3a26af551
volkan <16579273+mevolkan@users.noreply.github.com>
2024-05-31 04:11:51 +0300

    changed the layout

---
+++ frontend/src/app/components/maincontent.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+function Main() {
+    return (
+        <>
+            <div className="w-fit">
+                <label htmlFor="sidebar-mobile-fixed" className="btn-primary btn sm:hidden">Open Sidebar</label>
+            </div>
+
+            <div className="my-4 grid grid-cols-2 gap-4">
+                <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
+
+                <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
+                <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
+                <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
+                <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
+                <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
+                <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
+                <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
+                <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
+                <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
+            </div>
+        </>
+    );
+}
+
+export default Main;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,20 @@
 import Image from "next/image";
 import Sidebar from "./components/sidebar";
+import Main from "./components/maincontent";
 
 export default function Home() {
   return (
-    < >
-      <aside>
-        <Sidebar />
-      </aside>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <div className="flex flex-row sm:gap-10" >
+      <div className="sm:w-full sm:max-w-[18rem]">
+        <input type="checkbox" id="sidebar-mobile-fixed" className="sidebar-state" />
+        <label htmlFor="sidebar-mobile-fixed" className="sidebar-overlay"></label>
+        <aside>
+          <Sidebar />
+        </aside>
+      </div>
+      <main className="flex w-full flex-col p-4">
+        <Main />
       </main>
-    </>
+    </div>
   );
 }
