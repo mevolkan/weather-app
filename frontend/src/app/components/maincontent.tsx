79c583921ac56f9a21e62ce32a996617ce9e469a
volkan <16579273+mevolkan@users.noreply.github.com>
2024-05-31 22:43:02 +0300

    fixed the layout on mobile weird bug

--- frontend/src/app/components/maincontent.tsx
+++ frontend/src/app/components/maincontent.tsx
@@ -3,13 +3,9 @@
 function Main() {
     return (
         <div className="flex flex-col p-4">
-            <div className="w-fit">
-                <label htmlFor="sidebar-mobile-fixed" className="btn-primary btn sm:hidden">Open Sidebar</label>
-            </div>
 
             <div className="my-4 grid grid-cols-2 gap-4">
                 <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
-
                 <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
                 <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
                 <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
